91f9e07b03f3c015a07aa4f067ddc86655061936
Ganesh <110025487+ganeshbablu@users.noreply.github.com>
2022-07-29 16:08:35 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,15 @@
+class Program1
+{
+    public static void main(String[]args)
+    {
+    System.out.println("core java");
+    System.out.println("12345");  	
+    System.out.println("10+20"); 
+    System.out.println("true");
+    System.out.println("false");
+    System.out.println("10.20f"); 
+    System.out.println("0b0011");
+    System.out.println(30+30+"javaprogram"+10+30); 
+    System.out.println("end program"); 
+    } 
+}
